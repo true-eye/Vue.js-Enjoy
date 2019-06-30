d81b1e6090696f884569a6ece6a197afbbbb1fc9
AngularMaster <>
2019-06-30 14:58:15 -0400

    Second commit

--- src/main.js
+++ src/main.js
@@ -1,16 +1,18 @@
-import Vue from 'vue';
-import App from './App.vue';
-import VueRouter from 'vue-router';
+import Vue from "vue";
+import App from "./App.vue";
+import VueRouter from "vue-router";
 
-import routes from './routes';
+import routes from "./routes";
 
 Vue.config.productionTip = false;
 
 Vue.use(VueRouter);
 
-const router = new VueRouter({mode: 'history', routes});
+const router = new VueRouter({ mode: "history", routes });
+
+// Hello World!
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app');
+}).$mount("#app");
